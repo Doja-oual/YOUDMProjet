90bf004431252150ebd135a9601cd9e46eb0241d
Doja-oualla <dojaoualla@gmail.com>
2025-01-18 17:43:51 +0100

    ajoute methode de addEvaluation

--- App/Models/EvaluationRepository.php
+++ App/Models/EvaluationRepository.php
@@ -10,5 +10,25 @@ class EvaluationRepository {
         return Database::getConnection();
     }
 
+    public static function  addEvaluation($studentId,$coursId,$note,$comentaire){
+        $conn=self::getConnection();
+        $sql="INSERT INTO Evaluation(etudiant_id,cours_id,note,commentaire) 
+        VALUES (:etudiant_id,:cours_id,:note,commentaire)";
+
+        try{
+            $stmt = $conn->prepare($sql);
+            return $stmt->execute([
+                'etudiant_id' => $studentId,
+                'cours_id' => $courseId,
+                'note' => $note,
+                'commentaire' => $commentaire,
+            ]);
+                    }catch (PDOException $e) {
+                        error_log("Erreur lors de l'ajout de l'évaluation : " . $e->getMessage());
+                        return false;
+
+                    }
+    }
+
     
 }
