f32fc784cb9690f61528b2a29e954af61ce0b0a8
Doja-oualla <dojaoualla@gmail.com>
2025-01-18 17:09:10 +0100

    methode qui verifie inscription

--- App/Models/InscriptionRepository.php
+++ App/Models/InscriptionRepository.php
@@ -31,4 +31,26 @@ return false;
 
  }
 
+ // verifie que deja inscrite
+
+ public static function isInscrit($studentId,$courseId){
+    $conn=  self::getConnection();
+    $sql="SELECT *FROM Inscription WHERE etudiant_id=: etudiant_id AND cours_id=:cours_id";
+    try{
+        $stmt=$conn->prepare($sql);
+        stmt->execute([
+            'etudiant_id'=>$studentId,
+            'cours_id'=>$courseId,
+        ]);
+
+    }catch(PDOException $e){
+        error_log("Erreur lors de la verification de l'inscription :" . $e->getMessage());
+        return false;
+    }
+
+ }
+
+
+ 
+
 }
