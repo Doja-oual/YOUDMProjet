70ebd6026105be39887f0334c9ce305dbd83afba
Doja-oualla <dojaoualla@gmail.com>
2025-01-18 18:01:42 +0100

    ajoute claase  Cirtificatrepository pour methode de recupere les cirtificat d'un etudiant

---
+++ App/Models/CertificatRepository .php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Config\Database;
+use PDOException;
+
+class CertificatRepository{
+    private static function getConnection() {
+        return Database::getConnection();
+    }
+
+    // recuperer les cirtificats d'un etudiant
+
+    public static function  getCertificatresByStudent($student){
+        $conn = self::getConnection();
+        $sql="SELECT *FROM Certificat WHERE etudiant_id=:etudiant_id";
+
+        try{
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(['etudiant_id' => $studentId]);
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            error_log("Erreur lors de la récupération des certificats : " . $e->getMessage());
+            return false;
+        }
+    
+    }
+}
